7e44a5a02b076bf3da4721cff7a4ea6396895610
nmanjos <nmanjos@gmail.com>
2017-03-08 13:17:04 +0000

    08-03-2017

--- ocrexample/Form1.cs
+++ ocrexample/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace ocrexample
 {
--- ocrexample/Program.cs
+++ ocrexample/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System.Drawing;
+using System.Drawing.Imaging;
+using MosaiqPerformanceMonitor;
 
 namespace ocrexample
 {
@@ -12,11 +17,35 @@ namespace ocrexample
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        static IImage imageresizer(string path)
+        {
+            Mat frame = new Mat();
+            int width = 640, height = 480;
+            
+
+            frame = CvInvoke.Imread(path, ImreadModes.AnyColor);
+
+            CvInvoke.Resize(frame, frame, new Size(width, height), 0, 0, Inter.Linear);    //This resizes the image into your specified width and height
+
+            return frame;
+        }
+
+
+
+
         static void Main()
         {
+
+            // example  
+            var image = ScreenCapture.CaptureActiveWindow();
+            image.Save(@"C:\temp\snippetsource.jpg", ImageFormat.Jpeg);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
         }
     }
 }
