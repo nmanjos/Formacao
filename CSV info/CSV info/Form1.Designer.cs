8b5256d71ba017945dc19423b62bbfe5a6470047
nmanjos <nmanjos@gmail.com>
2017-04-05 15:51:13 +0100

    new proj

---
+++ CSV info/CSV info/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace CSV_info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNomeC = new System.Windows.Forms.TextBox();
+            this.txtMoradaC = new System.Windows.Forms.TextBox();
+            this.txtClientes = new System.Windows.Forms.RichTextBox();
+            this.txtContactoV = new System.Windows.Forms.TextBox();
+            this.txtNomeV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.btnADDV = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProfissaoC = new System.Windows.Forms.TextBox();
+            this.txtContactoC = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtMotivoV = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(7, 208);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(464, 43);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "READ CSV TODOS";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnADD
+            // 
+            this.btnADD.Location = new System.Drawing.Point(7, 593);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(464, 43);
+            this.btnADD.TabIndex = 3;
+            this.btnADD.Text = "ADD TO CSV";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "NOME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MORADA";
+            // 
+            // txtNomeC
+            // 
+            this.txtNomeC.Location = new System.Drawing.Point(7, 344);
+            this.txtNomeC.Name = "txtNomeC";
+            this.txtNomeC.Size = new System.Drawing.Size(464, 26);
+            this.txtNomeC.TabIndex = 6;
+            // 
+            // txtMoradaC
+            // 
+            this.txtMoradaC.Location = new System.Drawing.Point(7, 409);
+            this.txtMoradaC.Name = "txtMoradaC";
+            this.txtMoradaC.Size = new System.Drawing.Size(464, 26);
+            this.txtMoradaC.TabIndex = 7;
+            // 
+            // txtClientes
+            // 
+            this.txtClientes.Location = new System.Drawing.Point(7, 12);
+            this.txtClientes.Name = "txtClientes";
+            this.txtClientes.Size = new System.Drawing.Size(464, 190);
+            this.txtClientes.TabIndex = 8;
+            this.txtClientes.Text = "";
+            // 
+            // txtContactoV
+            // 
+            this.txtContactoV.Location = new System.Drawing.Point(480, 409);
+            this.txtContactoV.Name = "txtContactoV";
+            this.txtContactoV.Size = new System.Drawing.Size(464, 26);
+            this.txtContactoV.TabIndex = 13;
+            // 
+            // txtNomeV
+            // 
+            this.txtNomeV.Location = new System.Drawing.Point(480, 344);
+            this.txtNomeV.Name = "txtNomeV";
+            this.txtNomeV.Size = new System.Drawing.Size(464, 26);
+            this.txtNomeV.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(477, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "CONTACTO";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(479, 312);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(56, 20);
+            this.lblNome.TabIndex = 10;
+            this.lblNome.Text = "NOME";
+            // 
+            // btnADDV
+            // 
+            this.btnADDV.Location = new System.Drawing.Point(480, 593);
+            this.btnADDV.Name = "btnADDV";
+            this.btnADDV.Size = new System.Drawing.Size(464, 43);
+            this.btnADDV.TabIndex = 9;
+            this.btnADDV.Text = "ADD TO CSV";
+            this.btnADDV.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "CLIENTES";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(479, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "VISITANTES";
+            // 
+            // txtProfissaoC
+            // 
+            this.txtProfissaoC.Location = new System.Drawing.Point(7, 548);
+            this.txtProfissaoC.Name = "txtProfissaoC";
+            this.txtProfissaoC.Size = new System.Drawing.Size(464, 26);
+            this.txtProfissaoC.TabIndex = 19;
+            // 
+            // txtContactoC
+            // 
+            this.txtContactoC.Location = new System.Drawing.Point(7, 479);
+            this.txtContactoC.Name = "txtContactoC";
+            this.txtContactoC.Size = new System.Drawing.Size(464, 26);
+            this.txtContactoC.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 520);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "PROFISSÃO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 451);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "CONTACTO";
+            // 
+            // txtMotivoV
+            // 
+            this.txtMotivoV.Location = new System.Drawing.Point(480, 479);
+            this.txtMotivoV.Name = "txtMotivoV";
+            this.txtMotivoV.Size = new System.Drawing.Size(464, 26);
+            this.txtMotivoV.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(477, 451);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "MOTIVO";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(477, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(464, 190);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 43);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "READ CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(695, 217);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(128, 24);
+            this.radioButton1.TabIndex = 24;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "VISITANTES";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(829, 217);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 24);
+            this.radioButton2.TabIndex = 25;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "CLIENTES";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 648);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtMotivoV);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtProfissaoC);
+            this.Controls.Add(this.txtContactoC);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtContactoV);
+            this.Controls.Add(this.txtNomeV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnADDV);
+            this.Controls.Add(this.txtClientes);
+            this.Controls.Add(this.txtMoradaC);
+            this.Controls.Add(this.txtNomeC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNomeC;
+        private System.Windows.Forms.TextBox txtMoradaC;
+        private System.Windows.Forms.RichTextBox txtClientes;
+        private System.Windows.Forms.TextBox txtContactoV;
+        private System.Windows.Forms.TextBox txtNomeV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Button btnADDV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProfissaoC;
+        private System.Windows.Forms.TextBox txtContactoC;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMotivoV;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ CSV info/CSV info/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_info
+{
+    public partial class Form1 : Form
+    {
+
+        private const string TODOS = "TODOS";
+        private const string CLIENTES = "CLIENTES";
+        private const string VISITANTES = "VISITANTES";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<string[]> ReadCSVFile(string tipo)
+        {
+
+            FileStream fs = File.OpenRead(@"csvfile.csv");
+            StreamReader reader = new StreamReader(fs);
+            
+            List<string[]> list = new List<string[]>();
+
+            while (!reader.EndOfStream)
+            {   
+                //Decidir consoante o tipo os registos que interessam
+                string line = reader.ReadLine();
+                string[] values = line.Split(';');
+                list.Add(values);
+            }
+            reader.Close();
+            fs.Close();
+            return list;
+        }
+        public void WriteCSVFile(string nome, string morada, string contacto, string profissao)
+        {
+
+            FileStream fs = new FileStream(@"csvfile.csv", FileMode.Append, FileAccess.Write);
+            
+            StreamWriter writer = new StreamWriter(fs);
+            writer.WriteLine("{0};{1};{2};{3}", nome,morada,contacto, profissao);
+            writer.Close();
+            fs.Close();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            txtClientes.Text = ReadCSVFile(null)[0][0];
+            foreach (string[] item in ReadCSVFile(null))
+            {
+
+            }
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
