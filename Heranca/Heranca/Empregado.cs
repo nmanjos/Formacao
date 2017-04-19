4ecc5a91124e8ff86f78323e5a64c3d292b7e52e
nmanjos <nmanjos@gmail.com>
2017-04-19 15:34:41 +0100

    Herança

---
+++ Heranca/Heranca/Empregado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    class Empregado
+    {
+        private string nome;
+        private int idade;
+        public Empregado(String NomeDaPessoa, int IdadeDaPessoa):this(NomeDaPessoa)
+        {
+            
+            idade = IdadeDaPessoa;
+
+        }
+        public Empregado(String NomeDaPessoa)
+        {
+            nome = NomeDaPessoa;
+        }
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+        public int Idade
+        {
+            get
+            {
+                return idade;
+            }
+            set
+            {
+                idade = value;
+            }
+        }
+        public virtual String MostraFuncao()
+        {
+            return "Empregado";
+        }
+    }
+}
---
+++ Heranca/Heranca/Patrao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    class Patrao:Empregado
+    {
+        private int naccoes;
+        public Patrao(string NomeDoPatrao,int IdadeDoPatrao,int nAccoes)
+            :base(NomeDoPatrao,IdadeDoPatrao)
+        {
+            naccoes = nAccoes;
+        }
+
+        public override string MostraFuncao()
+        {
+            return base.MostraFuncao() + " - " + "Patrão";
+        }
+        public int MostraAccoes()
+        {
+            return naccoes;
+        }
+    }
+}
